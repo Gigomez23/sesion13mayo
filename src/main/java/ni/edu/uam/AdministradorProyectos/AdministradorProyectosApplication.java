7ee654a36e260b1f420fbb65b15307d998f54cdf
Gabriel Gómez <103473449+Gogogomez23@users.noreply.github.com>
2026-05-13 17:34:10 -0600

    primer commit

---
+++ src/main/java/ni/edu/uam/AdministradorProyectos/AdministradorProyectosApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.AdministradorProyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ni/edu/uam/AdministradorProyectos/AdministradorProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.AdministradorProyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
